3fb5325f96eef87a2793c6195ddef4b732bb4a21
PaulP <paul.petry83@gmail.com>
2017-03-05 17:32:45 +0000

    show avatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,6 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
-    
   end
   
   def update
@@ -27,6 +26,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   
   
   private
