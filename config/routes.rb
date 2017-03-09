3494ba4a51dd672672914091934b3bc0262aa70c
Paul Petry <paul.petry83@gmail.com>
2017-03-09 20:14:14 +0100

    push from mac

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,16 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
-  
+
   get 'signup', to: 'users#new'
  # post 'users', to: 'users#create'
   resources :users, except: [:new]
-  
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
