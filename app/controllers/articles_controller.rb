11db05d364b163ef90f1aeb19169a9f69025e2bc
Paul Petry <paul.petry83@gmail.com>
2017-03-10 22:30:58 +0100

    did more with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
   before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
-  
+
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
@@ -24,10 +24,10 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
   end
-  
+
   def update
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
@@ -36,22 +36,22 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @article.destroy
     flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
-  
+
   private
     def set_article
       @article = Article.find(params[:id])
     end
-    
+
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
    end
-   
+
    def require_same_user
     if current_user != @article.user and !current_user.admin?
       flash[:danger] = 'You can only edit or delete your own articles'
